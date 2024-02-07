8bfc6857730c68cb425fc041d49de29d670cfcfd
= <=>
2024-02-07 12:27:44 +0100

    Interfaces

--- Interface/IGUI.java
+++ Interface/IGUI.java
@@ -1,4 +1,13 @@
 package Interface;
 
+import Model.Entity.Favs;
+
 public interface IGUI {
+    void mostrarInicio();
+    void mostrarMenuPrincipal();
+    int mostrarMenuFavoritos();
+    int leeNumero();
+    String leeString();
+    Favs mostrarAñadirBorrarFavorito();
+
 }
--- Interface/ILibrary.java
+++ Interface/ILibrary.java
@@ -1,4 +1,10 @@
 package Interface;
 
+import Model.Entity.Favs;
+
 public interface ILibrary {
+    void listarFavorito(String nombre);
+    void listarFavoritos();
+    boolean añadirFavorito(Favs fav);
+    boolean borrarFavorito(Favs fav);
 }
